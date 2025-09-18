allnes/ITLabAI

commit bddbfda455542bd2faa68f3e074805e3acbb9d37

Optimize Output layer top-k implementation (#210)

include/layers/OutputLayer.hpp
@@ -104,7 +104,7 @@ std::pair<std::vector<std::string>, std::vector<ValueType>> top_k_vec(
   for (size_t i = 0; i < input.size(); i++) {
     sort_buf[i] = std::make_pair(labels[i], input[i]);
   }
-  std::sort(sort_buf.begin(), sort_buf.end(), compare_pair<ValueType>);
+  std::partial_sort(sort_buf.begin(), sort_buf.begin() + k, sort_buf.end(), compare_pair<ValueType>);
   std::vector<std::string> res_labels(k);
   std::vector<ValueType> res_input(k);
   for (size_t i = 0; i < k; i++) {

commit 58a652ef7ac9ca584191d77e853b3618a542df14

Apply hotfix for clang-format (#211)
Issue introduced in
https://github.com/embedded-dev-research/ITLabAI/pull/210

include/layers/OutputLayer.hpp
@@ -104,7 +104,8 @@ std::pair<std::vector<std::string>, std::vector<ValueType>> top_k_vec(
   for (size_t i = 0; i < input.size(); i++) {
     sort_buf[i] = std::make_pair(labels[i], input[i]);
   }
-  std::partial_sort(sort_buf.begin(), sort_buf.begin() + k, sort_buf.end(), compare_pair<ValueType>);
+  std::partial_sort(sort_buf.begin(), sort_buf.begin() + k, sort_buf.end(),
+                    compare_pair<ValueType>);
   std::vector<std::string> res_labels(k);
   std::vector<ValueType> res_input(k);
   for (size_t i = 0; i < k; i++) {